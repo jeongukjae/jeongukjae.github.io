934d82692d165a45bf27db1fb27a2e7eba92f008
Ukjae Jeong <jeongukjae@gmail.com>
2023-09-27 14:17:31 +0900

    add post

--- serve.sh
+++ serve.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-jekyll serve -lD
+docker build -f .devcontainer/Dockerfile -t jekyll .
+docker run --rm -it -p 4000:4000 -v $(pwd):/src -w /src jekyll jekyll serve --incremental --host 0.0.0.0
